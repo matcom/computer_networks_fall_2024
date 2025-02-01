da7e085246db47e2accd2da8d34aaad3df9ce26a
Joel0347 <127158086+Joel0347@users.noreply.github.com>
2025-02-01 12:06:11 -0500

    ajustado para nuevos parametros de los tests

--- proyecto/client.py
+++ proyecto/client.py
@@ -2,9 +2,8 @@ import socket
 import ssl
 import sys
 
-# Configuración del cliente FTP
 BUFFER_SIZE = 1024
-TIMEOUT = 30  # Ajustar tiempo de espera a 30 segundos
+TIMEOUT = 30
 
 def connect_to_server(server, port):
     context = ssl.create_default_context()
@@ -48,6 +47,8 @@ def send_command(client_socket, command):
 
 def list_files(tls_socket):
     response = send_command(tls_socket, 'PASV')
+
+    # Extraer la dirección IP y el puerto del modo pasivo
     start = response.find('(') + 1
     end = response.find(')')
     pasv_data = response[start:end].split(',')
@@ -56,21 +57,23 @@ def list_files(tls_socket):
 
     data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     data_socket.connect((ip, port))
+
     send_command(tls_socket, 'LIST')
 
+    # Leer la lista de archivos
     data = b""
     while True:
         chunk = data_socket.recv(BUFFER_SIZE)
         if not chunk:
             break
         data += chunk
-
     data_socket.close()
     print(data.decode())
 
 def stor_file(tls_socket, local_file, remote_file):
     send_command(tls_socket, 'TYPE I')
     response = send_command(tls_socket, 'PASV')
+
     start = response.find('(') + 1
     end = response.find(')')
     pasv_data = response[start:end].split(',')
@@ -79,6 +82,7 @@ def stor_file(tls_socket, local_file, remote_file):
 
     data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     data_socket.connect((ip, port))
+
     send_command(tls_socket, f'STOR {remote_file}')
 
     with open(local_file, 'rb') as file:
@@ -94,6 +98,7 @@ def stor_file(tls_socket, local_file, remote_file):
 def retr_file(tls_socket, remote_file, local_file):
     send_command(tls_socket, f'TYPE I')
     response = send_command(tls_socket, 'PASV')
+
     start = response.find('(') + 1
     end = response.find(')')
     pasv_data = response[start:end].split(',')
@@ -102,6 +107,7 @@ def retr_file(tls_socket, remote_file, local_file):
 
     data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     data_socket.connect((ip, port))
+
     send_command(tls_socket, f'RETR {remote_file}')
 
     with open(local_file, 'wb') as file:
@@ -115,41 +121,9 @@ def retr_file(tls_socket, remote_file, local_file):
     print(tls_socket.recv(BUFFER_SIZE).decode())
 
 def ftp_client(args):
-    # Inicializar variables para argumentos
-    server = None
-    port = None
-    user = None
-    password = None
-    command = None
-    arg1 = None
-    arg2 = None
-
-    # Procesar argumentos de sys.argv
-    i = 1  # iniciar en 1 para saltar el nombre del script
-    while i < len(args):
-        if args[i] == '-h':
-            server = args[i + 1]
-            i += 2
-        elif args[i] == '-p' and port is None:
-            port = int(args[i + 1])
-            i += 2
-        elif args[i] == '-u':
-            user = args[i + 1]
-            i += 2
-        elif args[i] == '-p' and password is None:
-            password = args[i + 1]
-            i += 2
-        elif args[i] == '-c':
-            command = args[i + 1]
-            i += 2
-        elif args[i] == '-a':
-            arg1 = args[i + 1]
-            i += 2
-        elif args[i] == '-b':
-            arg2 = args[i + 1]
-            i += 2
-        else:
-            i += 1
+    # Extraer servidor y puerto de los argumentos
+    server = args[args.index('-h') + 1]
+    port = int(args[args.index('-p') + 1])
 
     client_socket = connect_to_server(server, port)
     if not client_socket:
@@ -157,28 +131,47 @@ def ftp_client(args):
         return
 
     try:
+        user = args[args.index('-u') + 1]
+        password = args[args.index('-w') + 1]
+
         user_response = send_command(client_socket, f"USER {user}")
 
         if user_response and "331" in user_response:
             send_command(client_socket, f"PASS {password}")
 
-        if command:
-            command_name = command.upper()
-
-            if command_name == 'QUIT':
-                send_command(client_socket, command)
-            elif command_name == 'STOR':
-                stor_file(client_socket, arg1, arg2)
-            elif command_name == 'RETR':
-                retr_file(client_socket, arg1, arg2)
-            elif command_name == 'LIST':
+        if '-c' in args:
+            command = args[args.index('-c') + 1].upper()
+            if command == 'QUIT':
+                send_command(client_socket, 'QUIT')
+            elif command == 'PWD':
+                send_command(client_socket, 'PWD')
+            elif command == 'CWD':
+                directory = args[args.index('-a') + 1]
+                send_command(client_socket, f'CWD {directory}')
+            elif command == 'RETR':
+                remote_file = args[args.index('-a') + 1]
+                local_file = args[args.index('-b') + 1]
+                retr_file(client_socket, remote_file, local_file)
+            elif command == 'STOR':
+                local_file = args[args.index('-a') + 1]
+                remote_file = args[args.index('-b') + 1]
+                stor_file(client_socket, local_file, remote_file)
+            elif command == 'LIST':
                 list_files(client_socket)
-            elif command_name in ['USER', 'PASS', 'CWD', 'TYPE', 'STRU', 'MODE', 'APPE', 'DELE', 'RMD', 'MKD', 'PORT',
-                                  'RNFR', 'RNTO']:
-                full_command = f"{command} {arg1}" if arg1 else command
-                send_command(client_socket, full_command)
-            elif command_name in ['CDUP', 'PWD', 'PASV', 'SYST', 'HELP', 'NOOP', 'REIN']:
-                send_command(client_socket, command)
+            elif command == 'DELE':
+                file_to_delete = args[args.index('-a') + 1]
+                send_command(client_socket, f'DELE {file_to_delete}')
+            elif command == 'MKD':
+                directory_to_create = args[args.index('-a') + 1]
+                send_command(client_socket, f'MKD {directory_to_create}')
+            elif command == 'RMD':
+                directory_to_remove = args[args.index('-a') + 1]
+                send_command(client_socket, f'RMD {directory_to_remove}')
+            elif command == 'RNFR':
+                old_name = args[args.index('-a') + 1]
+                new_name = args[args.index('-b') + 1]
+                send_command(client_socket, f'RNFR {old_name}')
+                send_command(client_socket, f'RNTO {new_name}')
             else:
                 print("Comando no implementado o no reconocido.")
 
@@ -190,4 +183,5 @@ def ftp_client(args):
         print("Conexión cerrada.")
 
 if __name__ == "__main__":
-    ftp_client(sys.argv)
+    argvs = sys.argv[1:]
+    ftp_client(argvs)
