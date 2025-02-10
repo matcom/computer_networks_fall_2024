10a9e37deff9c0b00df6731e7000f043ea255a69
amabe418 <valienteamalia418@gmail.com>
2025-02-09 19:38:15 -0500

    Create chat_window.py

---
+++ src/chat_window.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from tkinter import font
+from datetime import datetime
+class ChatWindow:
+    def __init__(self, controller, name):
+        self.controller = controller
+        self.name = name
