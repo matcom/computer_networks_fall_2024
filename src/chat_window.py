e34b6e36dd9ba2f8c6a51fb35583ee18696c989b
amabe418 <valienteamalia418@gmail.com>
2025-02-09 19:39:50 -0500

    some necessary stuff implemented

--- src/chat_window.py
+++ src/chat_window.py
@@ -5,3 +5,31 @@ class ChatWindow:
     def __init__(self, controller, name):
         self.controller = controller
         self.name = name
+
+                # Create a new Tkinter window
+        self.window = tk.Toplevel()
+        self.window.title(name)
+
+        # Get the size of the default font
+        default_font = font.nametofont("TkDefaultFont")
+        default_size = default_font.cget("size")
+
+        # Create a font object with the default font, the default size, and a bold weight
+        bold_font = font.Font(size=default_size, weight='bold')
+
+        # Create a text widget for displaying messages
+        self.text_widget = tk.Text(self.window)
+        self.text_widget.pack()
+        self.text_widget.tag_configure('bold', font=bold_font)
+        self.text_widget.tag_configure('timestamp', foreground='blue')
+
+
+        # Create an entry widget for typing messages
+        self.entry_widget = tk.Entry(self.window)
+        self.entry_widget.pack(side=tk.BOTTOM, fill=tk.X)
+
+        # Bind the Return key to the send_message method
+        self.entry_widget.bind("<Return>", self.send_message)
+        self.window.protocol("WM_DELETE_WINDOW", self.on_close)
+
+
